5bff8a9dd13b208bd20ac76da46c5ed4c035f49a
Tayeb <tayeb.ez-zaoui@stadjutor.com>
2024-07-16 14:08:44 +0200

    synthesize the connection to the db

--- api/server.js
+++ api/server.js
@@ -1,44 +1,39 @@
-require('dotenv').config();
-
 const express = require('express');
 const app = new express();
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
-
-app.listen(3000, () => {
-    console.log("Server Listening");
-  });
-
-async function connection(){
-  await client.connect();
-  console.log('connection OK')
-  return 'done';
-}
-
-// Créer une nouvelle instance de MongoClient
 const client = new MongoClient(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Utilisation de la méthode connect pour se connecter à la base de données
-async function connection() {
-    await client.connect();
-    console.log('Connection OK');
-    return 'done!'
-  
+// connect to mangodb 
+async function connectionMangodb() {
+  await client.connect();
+  console.log('Connection OK');
+  return 'done!'
 }
-connection()
-    .then(console.log)
-    .catch(console.error)
-    .finally(() => client.close())
 
-    app.get('/products', async (req, res) => {
-      try {
-        const database = client.db();
-        const collection = database.collection('products');
-        const products = await collection.find({}).toArray();
-        res.json(products);
-      }
-      catch(err) {
-        console.error('Failed to fetch products', err);
-        res.status(500).send('Internal Server Error');
-      }
-    })
+// check db connection 
+function checkDb(){
+  connectionMangodb() 
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close())
+}
+
+app.get('/products', async (req, res) => {
+  try {
+    checkDb()
+    const database = client.db();
+    const collection = database.collection('products');
+    const products = await collection.find({}).toArray();
+    res.json(products);
+  }
+  catch(err) {
+    console.error('Failed to fetch products', err);
+    res.status(500).send('Internal Server Error');
+  }
+})
+
+app.listen(3000, () => {
+  console.log("Server Listening");
+});
