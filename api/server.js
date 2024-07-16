81c2bbb9d0333de32f4106329492911c5053f04a
Tayeb <tayeb.ez-zaoui@stadjutor.com>
2024-07-16 03:58:26 +0200

    add mongodb connection

--- api/server.js
+++ api/server.js
@@ -1,7 +1,31 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = new express();
-const mongo = require('mongodb');
+const { MongoClient } = require('mongodb');
+
 
 app.listen(3000, () => {
     console.log("Server Listening");
   });
+
+async function connection(){
+  await client.connect();
+  console.log('connection OK')
+  return 'done';
+}
+
+// Créer une nouvelle instance de MongoClient
+const client = new MongoClient(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Utilisation de la méthode connect pour se connecter à la base de données
+async function connection() {
+    await client.connect();
+    console.log('Connection OK');
+    return 'done!'
+  
+}
+connection()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close())
