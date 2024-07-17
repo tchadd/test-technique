930b28462e2dc9c658bf57374140d8dd05663519
Tayeb <tayeb.ez-zaoui@stadjutor.com>
2024-07-17 06:01:02 +0200

    install packages

--- front/vite-project/src/main.jsx
+++ front/vite-project/src/main.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+
+// Deuxième méthode de rendu (avec le composant App)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
